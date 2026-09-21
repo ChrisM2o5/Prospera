9be55e9b7b75fc962efa635f287032ccee3ee6e1
ChrisM2o5 <chris.marshall@slu.edu>
2026-09-21 03:06:14 +0000

    new file:  package.json new file:  vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+// @ts-ignore Dependencies are resolved when the Vite config is executed.
+import { defineConfig } from "vite";
+// @ts-ignore Dependencies are resolved when the Vite config is executed.
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+plugins: [react()],
+});
